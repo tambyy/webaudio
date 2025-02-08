fb1eb9bee0fe09208f3686a7aafb240edd5c2254
RAKOTONDRABE Tamby Roméo <tambyyr@gmail.com>
2025-02-08 09:23:20 +0300

    Création de tab layout

--- src/components/TabLayout.tsx
+++ src/components/TabLayout.tsx
@@ -1,31 +1,35 @@
 "use client";
 
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode } from "react";
 
-function Tabs<T>({
-  tabs,
-  render,
-  onChange,
-}: {
-  tabs: T[];
-  render: (tab: T, selected: boolean) => ReactNode;
-  onChange?: (i: number) => void;
-}) {
-  const [selected, setSelected] = useState(0);
+interface Tab {
+  id: string;
+  content: ReactNode;
+}
 
-  useEffect(() => {
-    if (onChange) onChange(selected);
-  }, [onChange, selected]);
-
+function TabLayout({ tab, tabs }: { tab: number; tabs: Tab[] }) {
   return (
-    <div className="w-full flex flex-row">
-      {tabs.map((tab, i) => (
-        <div className="cursor-pointer" key={i} onClick={() => setSelected(i)}>
-          {render(tab, i == selected)}
-        </div>
-      ))}
+    <div className="w-full h-full overflow-hidden flex flex-col">
+      <div
+        style={{
+          fontSize: 0,
+          width: 100 * tabs.length + "%",
+          marginLeft: -100 * tab + "%",
+        }}
+        className="overflow-hidden w-auto h-full whitespace-nowrap max-h-full duration-100 ease-in-out flex flex-row"
+      >
+        {tabs.map((tab: Tab) => (
+          <div
+            key={tab.id}
+            style={{ width: 100 / tabs.length + "%" }}
+            className="flex-1 font-xs inline-block max-h-full overflow-hidden whitespace-normal"
+          >
+            <div className="w-full h-full align-top">{tab.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default Tabs;
+export default TabLayout;
